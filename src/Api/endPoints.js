fb9ed0ad56668d62fb870c918da85d96ceb5c2e4
abz <teshalea000@gmail.com>
2025-06-26 07:37:53 +0300

    updated

--- src/Api/endPoints.js
+++ src/Api/endPoints.js
@@ -1,10 +1,1 @@
 export const productUrl = "https://fakestoreapi.com";
-// my netlify username:  https://ab-amazone.netlify.app/
-
-// the stripe secret key =sk_test_51RV9rTH76MuUflW2hfgak7GHwCQ3kblLr9BjzQ3GeDTdkScectzDHLP13wlUXIPkqZzNcvAjz9Sjvlp5lT8To5dg00l5e2tF58
-
-// the stripe public key==pk_test_51RV9rTH76MuUflW2YgllxQd2nolc7IJJsnIu3E78Y1KttgOAMNXjz8l5Zigw6bMsZwV6ilCXehgV1YbM1K3MFQbf00WKUfXrKt
-
-
-
-// necessary packages for the backend:npi express cors stripe dotenv
